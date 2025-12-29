a8a20db6daa05724bb10fcbc3228356bd8a27d7a
Joe Jaeger <joejaeger@Joes-Air.lan>
2025-12-29 15:10:53 -0600

    Fix: FeatureGrid client directive syntax

--- components/landing/FeatureGrid.tsx
+++ components/landing/FeatureGrid.tsx
@@ -1,7 +1,7 @@
-\"use client\";
+"use client";
 
-import { ReactNode } from \"react\";
-import { usePathname } from \"next/navigation\";
+import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
 
 export interface Feature {
   icon: ReactNode;
